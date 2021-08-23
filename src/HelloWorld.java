a5242bc556d44c76f2639b9479cfdfc8c68e58c3
MrElgheim <petter.elgheim@gmail.com>
2021-08-23 14:59:41 +0200

    Oppdatert filene

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
         for (int i=1;i<10;i++){
             System.out.println("Jeg telle norske"+" "+i);
         }
+        System.out.println("Petter også telle norske");
+        for (int i=11;i<20;i++){
+            System.out.println("Jeg telle norske"+" "+i);
+        }
     }
 
 }
